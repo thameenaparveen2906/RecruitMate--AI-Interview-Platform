a3d692c9102ff5b5f0df6b25b331541c598c231f
Thameena Parveen <thameenaparveenofficial@gmail.com>
2025-12-15 14:58:21 +0530

    Update ALLOWED_HOSTS for production settings

--- recruit_mate/recruit_mate/settings.py
+++ recruit_mate/recruit_mate/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://recruitmate-ai-interview-platform.onrender.com']
+ALLOWED_HOSTS = [
+    "recruitmate-ai-interview-platform.onrender.com",
+]
 
 
 # Application definition
